6ac650d63098e0feff924fd140c697936b05b448
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-09-13 12:03:21 -0600

    Update code.js
    fix sum function

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
 }
+console.log(sum([1,2,3]));
+//get help from instructor: npm install jsverify module
